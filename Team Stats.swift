6a0a4c9b8b81962e5fe5be82973c7954dce90bd6
Adrian <ajurczenia4219@stu.d214.org>
2023-12-06 11:12:26 -0600

    aaa

--- Team Stats.swift
+++ Team Stats.swift
@@ -1,36 +1,30 @@
 import Foundation
-import SwiftUI
 
+let headers = [
+    "X-RapidAPI-Key": "199b6f4b67msh57c679495124fccp15a497jsn324da79e9baa",
+    "X-RapidAPI-Host": "api-football-beta.p.rapidapi.com"
+]
 
-struct TeamStatsView: View {
-    var team: Team
+let request = NSMutableURLRequest(url: NSURL(string: "https://api-football-beta.p.rapidapi.com/teams/statistics?team=33&season=2019&league=39")! as URL,
+                                  cachePolicy: .useProtocolCachePolicy,
+                                  timeoutInterval: 10.0)
+request.httpMethod = "GET"
+request.allHTTPHeaderFields = headers
 
-    var body: some View {
-        VStack {
-            Text("\(team.name) Statistics")
-                .font(.headline)
-                .padding()
+let session = URLSession.shared
+let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+    if let error = error {
+        print("Error: \(error)")
+    } else {
+        if let httpResponse = response as? HTTPURLResponse {
+            print("Status Code: \(httpResponse.statusCode)")
 
-          
+            if let data = data {
+                // Handle the response data here
+                print("Response Data: \(String(data: data, encoding: .utf8) ?? "")")
+            }
         }
     }
-}
+})
 
-struct Team: Codable, Identifiable {
-    var id: Int
-    var name: String
-
-
-}
-
-struct StandingResponse: Codable {
-    var standings: [Standing]
-}
-
-struct Standing: Codable {
-    var table: [Team]
-}
-
-struct TeamStatsResponse: Codable {
-   
-}
+dataTask.resume()
